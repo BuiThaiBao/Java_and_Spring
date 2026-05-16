b8bbfe0ef1970e19c637a52b0cd12aeef5994250
buith <buithaibao2k4@gmail.com>
2026-05-16 16:40:17 +0700

    giai doan 0

---
+++ GiaiDoan00_ChuanBiMoiTruong/Bai3/Bai3.java
@@ -0,0 +1,13 @@
+package Bai3;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        System.out.println("=== BÀI 3: Thông tin hệ thống Java ===");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
+        System.out.println("Java Home: " + System.getProperty("java.home"));
+        System.out.println("OS Name: " + System.getProperty("os.name"));
+        System.out.println("OS Version: " + System.getProperty("os.version"));
+        System.out.println();
+    }
+}
---
+++ GiaiDoan00_ChuanBiMoiTruong/Bai5/Bai5.java
@@ -0,0 +1,11 @@
+package Bai5;
+
+public class Bai5 {
+    public static void main(String[] args) {
+        String name = "Bao";
+        int age = 22;
+        String school = "HUCE";
+        System.out.printf("Tên: %s, Tuổi: %d, Trường: %s", name, age, school);
+        System.out.println();
+    }
+}
